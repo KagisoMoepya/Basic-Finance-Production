2c60a2296c730492e440d3a50f18a47dc16b839e
kmoepya021 <kmoepya021@student.wethinkcode.co.za>
2022-08-04 00:21:42 +0200

    Completed Add New Category Design

--- Scripts/new_category.js
+++ Scripts/new_category.js
@@ -2,25 +2,45 @@
 * Adding a new category 
 */
 
-// Category Name
-let categoryName = document.getElementById("category_name");
+/**
+* On input a new name is set
+*/
 
-// Category Input
+let categoryName = document.getElementById("category_name");
 let categoryInput = document.getElementById("category_input");
 
-
-/**
- * On input the a new name is set
- */
-function newCategoryName() {
+categoryInput.addEventListener("input", () => {
 
     if (categoryInput.value == "") {
         categoryName.innerHTML = "New Category";
     } else {
         categoryName.innerHTML = categoryInput.value;
     }
-    
-}
 
-categoryInput.addEventListener("input", newCategoryName);
+});
+
+
+/**
+* Adds a picture on the picture view of Add New Category
+*/
+
+let categoryOrTypeImg = document.getElementById("category_or_type_img");
+let choosePicButton = document.getElementById("choose_img");
+let fileName = document.getElementById("file_name");
+let uploadedImage = "";
+
+choosePicButton.addEventListener("change", () => {
+    
+    console.log(choosePicButton.files);
+    const reader = new FileReader();
+
+    reader.onload = function() {
+        categoryOrTypeImg.src = reader.result;
+    }
+
+    const pictureData = choosePicButton.files[0];
+    reader.readAsDataURL(pictureData);
+    fileName.innerHTML = pictureData.name;
+    
+}, false)
 
