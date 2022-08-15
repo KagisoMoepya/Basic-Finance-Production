415ef4cddc9d79e630404587557b0c3d1a43e253
kmoepya021 <kmoepya021@student.wethinkcode.co.za>
2022-08-15 04:08:59 +0200

    Folder structure update

---
+++ client_side/Scripts/new_category.js
@@ -0,0 +1,79 @@
+/**
+* Adding a new category 
+*/
+
+/**
+* On input a new name is set
+*/
+
+let categoryName = document.getElementById("category_name")
+let categoryInput = document.getElementById("category_input")
+
+categoryInput.addEventListener("input", () => {
+
+    if (categoryInput.value == "") {
+        categoryName.innerHTML = "New Stock Item"
+    } else {
+        categoryName.innerHTML = categoryInput.value
+    }
+
+})
+
+/**
+* Adds a picture on the picture view of Add New Category
+*/
+
+let categoryOrTypeImg = document.getElementById("category_or_type_img")
+let choosePicButton = document.getElementById("choose_img")
+let fileName = document.getElementById("file_name")
+let fileNameString = ""
+
+choosePicButton.addEventListener("change", () => {
+    
+    // console.log(choosePicButton.files)
+    const reader = new FileReader()
+
+    reader.onload = function() {
+        categoryOrTypeImg.setAttribute("src", reader.result)
+        categoryOrTypeImg.style.backgroundColor = "#d5d1d1"
+    }
+
+    const pictureData = choosePicButton.files[0]
+    reader.readAsDataURL(pictureData)
+    fileName.innerHTML = pictureData.name
+    fileNameString = pictureData.name
+
+    
+    const files = choosePicButton.files
+    const formData = new FormData()
+    formData.append('myFile', files[0])
+
+    fetch('/Scripts', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+    
+}, false)
+
+/**
+* On Submit click color should change to standard color
+*/
+
+let addCategoryButton = document.getElementById("add_category_button")
+
+addCategoryButton.addEventListener("click", () => {
+
+    if (categoryInput.value.length > 0 && fileNameString != "") {
+        categoryOrTypeImg.style.backgroundColor = "#7B4E4E"
+    }
+
+})
+
+
